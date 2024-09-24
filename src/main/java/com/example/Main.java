b79a3aeaab484e0b249e210c62335991ec79a2cc
Mvrtn-design <martinmochi56@gmail.com>
2024-09-24 10:03:05 +0200

    haciendo el cherry

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -4,7 +4,6 @@ import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
-        LocalDate hoy = LocalDate.now();
-        System.out.println("This is the current date: " + hoy + ".\nStay well!");
+       System.out.println("Mensaje para hacer un chrry");
     }
 }
