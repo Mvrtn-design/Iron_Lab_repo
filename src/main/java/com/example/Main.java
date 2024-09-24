0afc7c4a4aed90adb5a4c7f2d4a84537e0e64fd1
Mvrtn-design <martinmochi56@gmail.com>
2024-09-24 09:23:54 +0200

    cambios para un mensaje mas detallado

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,8 +1,10 @@
 package com.example;
+
 import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
         LocalDate hoy = LocalDate.now();
-        System.out.println("Current date: " + hoy);
+        System.out.println("This is the current date: " + hoy + ".\nStay well!");
     }
 }
